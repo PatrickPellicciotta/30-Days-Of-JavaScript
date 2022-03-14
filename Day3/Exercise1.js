61be94a6161b937cff473bac4855c9c8faafe290
PatrickPellicciotta <patrickpellicciotta@gmail.com>
2022-03-14 04:49:43 -0400

    Added day3 folder with exercise1 file

---
+++ Day3/Exercise1.js
@@ -0,0 +1,39 @@
+const firstname = "Patrick"
+const lastname = "Pellicciotta"
+const city = "Laval"
+const age = 18
+const isMarried = "No"
+const year = 2022
+
+console.log(typeof firstname)
+console.log(typeof lastname)
+console.log(typeof city)
+console.log(typeof age)
+console.log(typeof isMarried)
+console.log(typeof year)
+
+console.log(typeof "10" == typeof 10)
+console.log(parseInt("9.8") == 10)
+
+//true statements
+console.log("1" == 1)
+console.log(3 > 2)
+console.log(10 < 42)
+
+//false statements
+console.log("1" === 1)
+console.log(90 < 10)
+console.log(200 < 0)
+
+ console.log(4 > 3) //true
+ console.log(4 >= 3) //true
+ console.log(4 < 3) //false
+ console.log(4 <= 3) //false
+ console.log(4 == 4) //true
+ console.log(4 === 4) //true
+ console.log(4 != 4) //false
+ console.log(4 !== 4) //false
+ console.log(4 != '4') //false
+ console.log(4 == '4') //true
+ console.log(4 === '4') //true
+ console.log("python".length != "jargon".length)
