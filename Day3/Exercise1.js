f9496d392c18475b7bb1339ad0de5bad7cdb19fc
PatrickPellicciotta <patrickpellicciotta@gmail.com>
2022-03-15 02:06:15 -0400

    finish exersice 1 started exersice 2

--- Day3/Exercise1.js
+++ Day3/Exercise1.js
@@ -37,3 +37,24 @@ console.log(200 < 0)
  console.log(4 == '4') //true
  console.log(4 === '4') //true
  console.log("python".length != "jargon".length)
+ 
+console.log(4 > 3 && 10 < 12) //true
+console.log(4 > 3 && 10 > 12) //false
+console.log(4 > 3 || 10 < 12)  //true
+console.log(4 > 3 || 10 > 12) //true
+console.log(!(4 > 3)) //false
+console.log(!(4 < 3)) //true
+console.log(!(false)) //true
+console.log(!(4 > 3 && 10 < 12)) //false
+console.log(!(4 > 3 && 10 > 12)) //true
+console.log(!(4 === '4')) //true
+console.log(!("dragon".length === "python".length))
+
+const now = new Date()
+console.log(now.getFullYear())
+console.log(now.getMonth(0-11))
+console.log(now.getDate())
+console.log(now.getDay(0-6))
+console.log(now.getHours())
+console.log(now.getMinutes())
+console.log(now.getTime())
---
+++ Day3/Exersice2.js
@@ -0,0 +1,9 @@
+let base = 20
+let height = 10
+console.log(0.5 * base * height)
+
+let a = 5
+let b = 4
+let c = 3
+console.log(a += b += c)
+//3
