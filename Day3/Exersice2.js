f8113c6e2d394114a06b42b0527364c2a79dbe36
PatrickPellicciotta <patrickpellicciotta@gmail.com>
2022-03-16 15:02:43 -0400

    ez

--- Day3/Exersice2.js
+++ Day3/Exersice2.js
@@ -2,8 +2,10 @@ let base = 20
 let height = 10
 console.log(0.5 * base * height)
 
-let a = 5
-let b = 4
-let c = 3
-console.log(a += b += c)
-//3
+let a = parseFloat(prompt("5"))
+let b = parseFloat(prompt("4"))
+let c = parseFloat(prompt("3"))
+console.log(a + b + c)
+
+let length = parseFloat(prompt("Length"))
+let width = parseFloat(prompt("Width"))
